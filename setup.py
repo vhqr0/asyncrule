0df3bc220cd9fd7da8592ff53c50c44836c88e0e
vhqr0 <zq_cmd@163.com>
2023-09-01 08:43:36 +0800

    setup: use compatible requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 
 import setuptools
 
+dependence = ['hy ~= 0.27.0', 'hyrule ~= 0.4.0']
+
 setuptools.setup(
     name='asyncrule',
     version='1.0.0',
     author='vhqr',
     description='async related macros for hylang',
-    install_requires=['hy == 0.27.0', 'hyrule == 0.4.0'],
+    install_requires=dependence,
     py_modules=['asyncrule'],
 )
